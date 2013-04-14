bbeda0367c16767c0a28810a2854f1fcfbc30d0a
John O. Brickley <obryan.brickley@gmail.com>
2013-04-14 14:49:58 -0800

    Updated README and fixed up the script.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ goog2ppl
 ========
 
 A Python script to convert Google Contacts to a format readable by [ppl](http://ppladdressbook.org/).
+
+Takes a vcf files and splits it into separate cards named after after the
+camelcase contents of their `N:` (name) lines. 
+
+##Caveats
+* Hardly tested, but works for me. So backup first!
+* Outputs bunches of vcf files to the directory from which it's run.
+
+##Usage
+From an initialized ppl directory:
+1. `./goog2ppl <contacts file>` to make cards for your contacts.
+2. `mv <contacts file> <contacts file>.bak` because I don't like it when people
+get mad at me.
+3. `git add *.vcf && git commit -a` to make the imported contacts visible
+to ppl.
--- goog2ppl.py
+++ goog2ppl.py
@@ -6,20 +6,23 @@ import sys
 # Gets first command line argument
 source = open(sys.argv[1])
 
+contact = ''
 outCard = []
 
 cardNum = 0
 for line in source:
     if not re.search(r'END:VCARD', line):
-        outCard.append(line)
+        # Is it a N: line?
         if re.search(r'(N:)(.+)(;;;)', line):
-            contact = re.sub(r'(N:)(.+)(;;;)', r'\2.vcf', line)
-            contact = contact.replace(';','')
+            contact = (re.sub(r'(N:)(.+)(;;;)', r'\2', line)).replace(';','')
             if re.search(r'(N:)(;;;)', line):
-                contact = 'MISSING CONTACT FIELD ' + str(cardNum) + contact
-        fileName = open(contact, 'w')
-    elif re.search(r'END:VCARD', line):
-        outCard.append(line)
+                contact = 'MISSING CONTACT FIELD ' + str(cardNum)
+            # Get rid of extra newlines.
+            contact = contact.rstrip()
+            line = (re.sub(r'(N:)(.+)(;;;)', r'\1%s\3' % contact, line))
+    outCard.append(line)
+    if re.search(r'END:VCARD', line):
+        fileName = open(contact + '.vcf', 'w')
         for line in outCard:
             fileName.write("%s" % line)
         outCard = []
